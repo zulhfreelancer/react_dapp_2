f62cbe235df9676b6b7f2f6f69a541804ad06bad
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-05-15 11:05:07 +0000

    Manage a lot of edge cases

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import web3App from './helpers/web3';
 
 class App extends Component {
   constructor(props) {
-    super();
-
+    super(props);
     this.state = {
       web3: null,
       isWeb3: false,
@@ -15,7 +14,9 @@ class App extends Component {
       balanceRaw: null,
       prettyBal: null,
       isDeployed: false,
-      contractAddress: null
+      contractAddress: null,
+      isNodeDown: true,
+      nodeStartedCount: 0
     }
   }
 
@@ -23,6 +24,8 @@ class App extends Component {
   * Lifecycle methods
   -------------------------------------------------- */
   componentDidMount() {
+    // console.clear();
+    console.log('componentDidMount');
     this.preBootApp();
   }
 
@@ -40,41 +43,101 @@ class App extends Component {
       isMetaMask: isMetaMask
     });
 
-    this.bootApp();
+    if (!isMetaMask) {
+      return this.setState({
+        error: "Sorry, we only support MetaMask. Please install it and refresh this page.",
+        isLoading: false
+      });
+    }
+
+    await this.bootApp();
   }
 
   async bootApp() {
-    let isConnected  = web3App.isConnected();
+    console.log('bootApp');
+    let isConnected  = web3App.isConnected;
     let isMetaMask   = this.state.isMetaMask;
     let node_err_msg = "Node is down. Please switch to another node in MetaMask settings.";
-    if (!isConnected && isMetaMask) return this.setState({isLoading: false, error: node_err_msg});
 
-    this.updateAccountAndContract();
+    if (!isConnected && isMetaMask) {
+      return this.setState({isLoading: false, error: node_err_msg});
+    }
+
+    this.listenNodeHeartbeat();
+    setInterval(() => this.listenNodeHeartbeat(), 1000);
+
     this.listenAccountChange();
   }
 
   async updateAccountAndContract() {
+    console.log('updateAccountAndContract');
+    await web3App.getContractInstance();
+
     let accounts        = await web3App.getAccounts();
+    // console.log('updateAccountAndContract.accounts', accounts);
     let isDeployed      = web3App.isContractDeployed;
+    console.log('updateAccountAndContract.isDeployed', isDeployed);
     let contractAddress = await web3App.getContractAddress();
+
+    if (accounts.length === 0) {
+      return this.showMetaMaskIsLockedError();
+    }
+
+    if (!isDeployed) {
+      let msg = "Contract is not deployed on this network. "
+      if (window.location.port === "3000") {
+        msg +=
+        "If you just deploy it, switch to Main network and switch back to Custom network in MetaMask"
+      }
+      return this.setState({
+        error: msg,
+        isLoading: false
+      });
+    }
+
+    console.log('after return');
 
     this.setState({
       accounts: accounts,
       isDeployed: isDeployed,
       contractAddress: contractAddress,
       isLoading: false,
+      error: null
     });
 
     this.fetchBalance();
   }
 
-  // this polling function will get called we user login/logout/switch account
+  showMetaMaskIsLockedError() {
+    const errorMsg = "Please unlock your MetaMask.";
+
+    // avoid useless state update
+    if (this.state.error !== errorMsg) {
+      this.setState({
+        accounts: [],
+        error: errorMsg,
+        isLoading: false
+      });
+    }
+  }
+
+  // this polling function will get called when user login/logout/switch account
   // we don't have subscribe/event yet for this - see: http://bit.ly/2HDTCZM
   listenAccountChange() {
+    // console.log('listenAccountChange');
     if (!this.state.isMetaMask) return;
     const self = this;
     setInterval(async function() {
-      if (await web3App.getAccount(0) !== self.state.accounts[0]) {
+      const firstAccount = await web3App.getAccount(0);
+      const stateAccount = self.state.accounts[0];
+      // console.log(firstAccount, stateAccount);
+
+      if (typeof firstAccount === 'undefined') {
+        return self.showMetaMaskIsLockedError();
+      }
+
+      if (firstAccount !== stateAccount) {
+        console.log('account changed');
         self.setState({ accounts: await web3App.getAccounts() }, function() {
           self.updateAccountAndContract();
         });
@@ -82,9 +145,35 @@ class App extends Component {
     }, 1000);
   }
 
+  async listenNodeHeartbeat() {
+    const self = this;
+    const errorMsg = 'The node is down';
+    let netId  = await web3App.getNetworkId();
+    // console.log('netId', netId, 'error', self.state.error);
+    if (netId === 0 && self.state.error !== errorMsg) {
+      console.log('node is down');
+      return self.setState({error: errorMsg, isLoading: false, isNodeDown: true});
+    }
+
+    if (netId !== 0 && self.state.isNodeDown) {
+      console.log('node is up');
+      const nsc = self.state.nodeStartedCount + 1;
+      self.setState(
+        {isLoading: true, isNodeDown: false, nodeStartedCount: nsc},
+        function() {
+          if (self.state.nodeStartedCount > 1) {
+            self.setState({error: 'Node is back up. Please re-login your MetaMask.', isLoading: false})
+          }
+        }
+      );
+    }
+  }
+
   async fetchBalance() {
+    console.log('fetchBalance');
     if (!this.state.isDeployed) return;
     if (this.state.accounts.length === 0) return;
+    if (this.state.error) return;
 
     let balanceRaw = await web3App.getTokenBalance('raw');
     let prettyBal  = await web3App.getTokenBalance(null);
@@ -94,12 +183,10 @@ class App extends Component {
   render() {
     const {
       isLoading,
-      isMetaMask,
       accounts,
       error,
       balanceRaw,
       prettyBal,
-      isDeployed,
       contractAddress
     } = this.state;
 
@@ -119,33 +206,10 @@ class App extends Component {
       </center>
     }
 
-    if (!isMetaMask) {
-      return <center>
-        <p>
-          Sorry, we only support MetaMask at this moment. Please install it and refresh this page.
-        </p>
-      </center>;
-    }
-
-    if (accounts.length === 0) {
-      return <center>
-        <p>
-          Please unlock your MetaMask.
-        </p>
-      </center>;
-    }
-
-    if (!isDeployed) {
-      return <center>
-        <p>
-          Seems like the contract is not deployed on this network.
-        </p>
-      </center>;
-    }
+    // console.log(this.state);
 
     return (
       <div className="container">
-        <p>Hello!</p>
         <p><b>Contract Address</b></p>
         <p>{contractAddress}</p>
         <p><b>Active Account</b></p>
--- src/helpers/web3.js
+++ src/helpers/web3.js
@@ -10,23 +10,57 @@ let web3App = {
   isContractDeployed: false,
 
   init: async function() {
+    console.log('----------- init -----------');
+
+    // setting default module states
+    web3App.instance           = null;
+    web3App.contracts          = {};
+    web3App.isContractDeployed = false;
+
     await web3App.initWeb3();
     await web3App.initContract('TutorialToken');
+    web3App.checkConnection(); // not an `await` because it's not a promise
     await web3App.checkContract();
   },
 
+  // `web3App.web3` is `window.web3`
   initWeb3: function() {
     if (typeof web3App.web3 !== 'undefined') {
+      // setting provider string
       web3App.web3Provider = web3App.web3.currentProvider;
+
+      // setting the web3 instance
       web3App.instance = new Web3(web3App.web3.currentProvider);
+
+      // console.log('web3 instance successfully created');
+    } else {
+      // console.log('web3 instance is null');
     }
   },
 
   initContract: async function(tokenName) {
+    if (!web3App.instance) return;
     let res = await fetch(`./abi/${tokenName}.json`);
     let abi = await res.json();
+    // console.log('abi', abi);
     web3App.contracts.TutorialToken = TruffleContract(abi);
     web3App.contracts.TutorialToken.setProvider(web3App.web3Provider);
+  },
+
+  getNetworkId: function() {
+    return new Promise(function(resolve, reject){
+      if (!web3App.instance) {
+        resolve(0);
+      }
+
+      web3App.instance.eth.net.getId()
+        .then(function(data){
+          resolve(data);
+        })
+        .catch(function(err){
+          resolve(0);
+        })
+    })
   },
 
   getContractInstance: async function() {
@@ -35,6 +69,7 @@ let web3App = {
       return contractInstance;
     } catch(error) {
       console.log('getContractInstance error');
+      web3App.isContractDeployed = false;
     }
   },
 
@@ -76,7 +111,7 @@ let web3App = {
       let instance = await web3App.getContractInstance();
       return instance.address;
     } catch(error) {
-      console.log('getContractAddress error');
+      // console.log('getContractAddress error');
     }
   },
 
@@ -92,20 +127,25 @@ let web3App = {
     }
   },
 
-  isConnected: function() {
+  checkConnection: function() {
     if (!web3App.instance) {
-      return false;
+      // console.log('checkConnection.1');
+      return web3App.isConnected = false;
     }
 
+    // e.g. Mist users
     if (web3App.instance.currentProvider.constructor.name !== 'MetamaskInpageProvider') {
-      return false; // e.g. Mist users
+      // console.log('checkConnection.2');
+      return web3App.isConnected = false;
     }
 
     // not the best solution - https://github.com/MetaMask/metamask-extension/issues/4126
-    if (web3App.instance.currentProvider.publicConfigStore._state.networkVersion === 'loading') {
-      return false;
+    if (web3App.instance.currentProvider.publicConfigStore._state.networkVersion !== 'loading') {
+      // console.log('checkConnection.3');
+      web3App.isConnected = true;
     } else {
-      return true;
+      // console.log('checkConnection.4');
+      web3App.isConnected = false;
     }
   },
 
@@ -116,19 +156,25 @@ let web3App = {
   *   3. switch back to Private Network
   */
   checkContract: function() {
-    if (!web3App.isConnected()) return;
-    return new Promise(async function(resolve, reject){
-      try {
-        let address  = await web3App.getContractAddress();
-        if (!address) return resolve(); // address is null
-        let byteCode = await web3App.instance.eth.getCode(address);
-        let result   = byteCode !== '0x0';
-        result ? web3App.isContractDeployed = true : web3App.isContractDeployed = false;
-        resolve();
-      } catch(error) {
-        console.log('checkContract error');
-        reject();
-      }
+    console.log('I get called inside checkContract');
+    if (!web3App.isConnected) return;
+    return new Promise(function(resolve, reject){
+      console.log('I get called inside checkContract promise block');
+      web3App.getContractAddress()
+        .then(function(address){
+          if (!address) return resolve(); // address is null
+          console.log('contract address', address);
+          return web3App.instance.eth.getCode(address);
+        })
+        .then(function(byteCode){
+          let result = byteCode !== '0x0';
+          // console.log('byteCode', byteCode, 'result', result);
+          result ? web3App.isContractDeployed = true : web3App.isContractDeployed = false;
+          resolve();
+        })
+        .catch(function(err){
+          reject();
+        })
     })
   },
 
